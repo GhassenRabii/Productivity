06809c215da89aa1327e74c4bc378ce08c9b05fb
Ghassen Rabii <rabaighassen0@gmail.com>
2025-07-26 17:55:04 +0200

    Update serializers.py

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,25 +2,41 @@ from rest_framework import serializers
 from .models import Task, Habit, Note, Event
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)  # Shows username in API
     class Meta:
         model = Task
-        fields = ['id', 'title', 'completed', 'user']
-        read_only_fields = ['user']
+        fields = [
+            'id', 'title', 'completed', 'due_date', 'priority', 'created',
+            'last_modified', 'tags', 'recurring', 'notes', 'user'
+        ]
+        read_only_fields = ['user', 'created', 'last_modified']
 
 class HabitSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Habit
-        fields = ['id', 'name', 'frequency', 'last_done', 'user']
-        read_only_fields = ['user']
+        fields = [
+            'id', 'name', 'frequency', 'last_done', 'streak',
+            'created', 'last_modified', 'notes', 'user'
+        ]
+        read_only_fields = ['user', 'created', 'last_modified', 'streak']
 
 class NoteSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Note
-        fields = ['id', 'title', 'content', 'created', 'user']
-        read_only_fields = ['user', 'created']
+        fields = [
+            'id', 'title', 'content', 'created',
+            'last_modified', 'tags', 'user'
+        ]
+        read_only_fields = ['user', 'created', 'last_modified']
 
 class EventSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Event
-        fields = ['id', 'title', 'event_date', 'description', 'user']
-        read_only_fields = ['user']
+        fields = [
+            'id', 'title', 'event_date', 'location', 'description',
+            'reminder', 'created', 'last_modified', 'tags', 'user'
+        ]
+        read_only_fields = ['user', 'created', 'last_modified']

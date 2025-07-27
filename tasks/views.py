b20ec9d035fd8c5c4153c36bdb653dcf4e09741d
Ghassen Rabii <rabaighassen0@gmail.com>
2025-07-27 20:55:44 +0200

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.core.paginator import Paginator
 from .models import Task, Habit, Note, Event
 from .forms import TaskForm, HabitForm, NoteForm, EventForm 

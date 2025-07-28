6e820a0d4156553f17825f3e1cecad03e3449770
Ghassen Rabii <rabaighassen0@gmail.com>
2025-07-28 14:26:01 +0200

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -19,7 +19,7 @@ app_name = "tasks"
 urlpatterns = [
     # HTML views
     path('no-access/', TemplateView.as_view(template_name='no_access.html'), name='no-access'),
-    path('', root_redirect, name='root-redirect'),
+    path('', views.task_list, name='task_list')
     path('new/', views.task_create, name='task_create'),
     path('habits/', views.habit_list, name='habit_list'),
     path('habits/new/', views.habit_create, name='habit_create'),

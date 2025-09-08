224420ccb9ac36f47b0f21926d892452b791c106
Ghassen Rabii <rabaighassen0@gmail.com>
2025-09-08 14:05:13 +0200

    Update signals.py

--- tasks/signals.py
+++ tasks/signals.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-    else:
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    # Only if you have extra fields that mirror user fields
+    if hasattr(instance, "profile"):
         instance.profile.save()

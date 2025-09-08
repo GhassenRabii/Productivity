abeb6a0598b7ca6940968d6bf0f17dff611794b3
Ghassen Rabii <rabaighassen0@gmail.com>
2025-09-08 14:04:41 +0200

    Update apps.py

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,6 +1,12 @@
 
 from django.apps import AppConfig
 
+class AccountsConfig(AppConfig):
+    name = "accounts"
+    
+    def ready(self):
+        from . import signals  
+
 class TasksConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tasks"

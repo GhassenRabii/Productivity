8ddeb31080860a07864faba4370a36cd46e53f43
Ghassen Rabii <rabaighassen0@gmail.com>
2025-09-09 13:34:13 +0200

    Update signals.py

--- tasks/signals.py
+++ tasks/signals.py
@@ -1,8 +1,17 @@
 from django.conf import settings 
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group   
 from django.dispatch import receiver
 from .models import Profile
+
+DEFAULT_GROUP_NAME = "users"
+
+@receiver(post_save, sender=User)
+def add_user_to_default_group(sender, instance, created, **kwargs):
+    if not created:
+        return
+    group, _ = Group.objects.get_or_create(name=DEFAULT_GROUP_NAME)
+    instance.groups.add(group)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
